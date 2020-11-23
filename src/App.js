acb20811e8c1b4dbf4292ec9c15965897dfe76f4
nitin gupta <nitingupta40@gmail.com>
2020-11-23 13:20:43 +1100

    Added cards
    added new repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Cards from './cards/Cards';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='pt-4 pl-4 pr-4 bg-light'>
+      <h4 className='text-danger ml-4'> Related Articles</h4>
+      <div className='row mb-4 ml-2'>
+        <div className='col-md-8'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem rerum tempore ad deserunt temporibus maiores quisquam
+          quia odit laborum cumque, nesciunt est similique, recusandae culpa
+          accusamus nobis dolorem possimus explicabo? Magni quod optio sunt
+          nulla assumenda, alias itaque rem veniam!
+        </div>
+      </div>
+      <Cards></Cards>
     </div>
   );
 }
